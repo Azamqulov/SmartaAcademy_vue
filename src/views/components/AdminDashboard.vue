817fd2a3b40c1ac7827a4851249ba61c966ae15a
Azamqulov Musurmon <140374591+Azamqulov@users.noreply.github.com>
2024-12-25 20:56:04 +0500

    ds

--- src/views/components/AdminDashboard.vue
+++ src/views/components/AdminDashboard.vue
@@ -4,14 +4,18 @@
     <v-row class="my-5">
       <v-col cols="12" md="4">
         <v-card class="text-h5 text-center" outlined>
-          <v-card-title >Jami O'quvchilar</v-card-title>
-          <v-card-subtitle class="text-h5">{{ totalStudents }} ta</v-card-subtitle>
+          <v-card-title>Jami O'quvchilar</v-card-title>
+          <v-card-subtitle class="text-h5"
+            >{{ totalStudents }} ta</v-card-subtitle
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
         <v-card class="text-h5 text-center" outlined>
           <v-card-title>Jami To'lovlar</v-card-title>
-          <v-card-subtitle class="text-h5">{{ totalPayments }} so'm</v-card-subtitle>
+          <v-card-subtitle class="text-h5"
+            >{{ totalPayments }} so'm</v-card-subtitle
+          >
         </v-card>
       </v-col>
       <v-col cols="12" md="4">
@@ -21,14 +25,15 @@
         </v-card>
       </v-col>
     </v-row>
+    <!-- ******* -->
     <DashboardOverview class="mt-5"></DashboardOverview>
+    <!-- *******  -->
   </v-container>
 </template>
   
   <script>
 import { db } from "@/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
-
 export default {
   data() {
     return {
@@ -37,7 +42,6 @@ export default {
       totalPass: 0, // Total students who passed
     };
   },
-
   methods: {
     // Fetch the total number of students, total payments, and passed students
     async fetchData() {
@@ -49,30 +53,25 @@ export default {
         const teacherCollection = collection(db, "teachers");
         const teachersSnapshot = await getDocs(teacherCollection);
         const teacher = teachersSnapshot.docs.map((doc) => doc.data());
-
         // Calculate total students
         this.totalStudents = students.length;
-        this.totalPass = teacher.length
+        this.totalPass = teacher.length;
         // Barcha to'lovlar summasini hisoblash
         let totalPayments = 0;
         studentsSnapshot.forEach((doc) => {
           const studentData = doc.data();
           let paymentValue = studentData.payment || 0;
-
           // Agar payment qiymati string bo'lsa, uni raqamga aylantirish
           if (typeof paymentValue === "string") {
             paymentValue = paymentValue.replace(/[^\d.-]/g, ""); // Faqat raqamlar va nuqtalarni qoldirib, qolganini olib tashlash
           }
-
           // Payment qiymatini raqamga aylantirish
           const parsedPayment = parseFloat(paymentValue);
-
           // Agar raqamga aylantirish muvaffaqiyatli bo'lsa, totalPayments ga qo'shish
           if (!isNaN(parsedPayment)) {
             totalPayments += parsedPayment;
           }
         });
-
         // To'lovlar summasini saqlash
         this.totalPayments = totalPayments;
       } catch (error) {
@@ -80,7 +79,6 @@ export default {
       }
     },
   },
-
   // Fetch data when the component is created
   created() {
     this.fetchData();
